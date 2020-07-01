e5dd82845301857dcfc6b703c61fd264b04e2f45
haller19 <haller19web@gmail.com>
2020-07-01 23:17:03 +0900

    background

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
+// [定数] webpack の出力オプションを指定します
+// 'production' か 'development' を指定
+// モード値を production に設定すると最適化された状態で、
+// development に設定するとソースマップ有効でJSファイルが出力される
+const MODE = "development";
+// ソースマップの利用有無(productionのときはソースマップを利用しない)
+const enabledSourceMap = MODE === "development";
+
 module.exports = {
-	// モード値を production に設定すると最適化された状態で、
-	// development に設定するとソースマップ有効でJSファイルが出力される
-	mode: "production",
+	// メインとなるJavaScriptファイル（エントリーポイント）
+	entry: `./src/index.js`,
+	// ファイルの出力設定
+	output: {
+		//  出力ファイルのディレクトリ名
+		path: `${__dirname}/dist`,
+		// 出力ファイル名
+		filename: "main.js",
+	},
+	mode: MODE,
 	module: {
 		rules: [
 			{
@@ -19,6 +34,9 @@ module.exports = {
 							url: false,
 							// ソースマップの利用有無
 							sourceMap: true,
+							// 0 => no loaders (default);
+							// 1 => postcss-loader;
+							// 2 => postcss-loader, sass-loader
 							// Sass+PostCSSの場合は2を指定
 							importLoaders: 2,
 						},
@@ -38,10 +56,16 @@ module.exports = {
 						loader: "sass-loader",
 						options: {
 							// ソースマップの利用有無
-							sourceMap: true,
+							sourceMap: enabledSourceMap,
 						},
 					},
 				]),
+			},
+			{
+				// 対象となるファイルの拡張子
+				test: /\.(gif|png|jpg|eot|wof|woff|woff2|ttf|svg)$/,
+				// 画像をBase64として取り込む
+				loader: "url-loader",
 			},
 		],
 	},
